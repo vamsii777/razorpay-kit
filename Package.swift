2e3f3e7f8ba584922348e9f7232ee2951f54c252
vamsii777 <vamsi@dewonderstruck.com>
2024-07-31 15:57:19 +0530

    Update async-http-client dependency to version 1.21.2

--- Package.swift
+++ Package.swift
@@ -10,15 +10,13 @@ let package = Package(
     ],
     products: [.library(name: "RazorpayKit", targets: ["RazorpayKit"])],
     dependencies: [
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.20.1"),
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "3.1.0")
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.21.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(name: "RazorpayKit", dependencies: [
-            .product(name: "AsyncHTTPClient", package: "async-http-client"),
-            .product(name: "Crypto", package: "swift-crypto"),
+            .product(name: "AsyncHTTPClient", package: "async-http-client")
         ]),
         .testTarget(
             name: "RazorpayKitTests",
